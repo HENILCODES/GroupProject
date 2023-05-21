cfc7feea2d1ea2cd67d3802872128082624468d0
henilcodes <henilcode@gmail.com>
2023-05-21 15:59:41 +0530

    create modal and migration

--- JJKirana/database/migrations/2023_05_21_082621_create_categories_table.php
+++ JJKirana/database/migrations/2023_05_21_082621_create_categories_table.php
@@ -11,14 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->enum('role', [0, 1, 2])->comment('0 : user | 1 : admin | 2: super admin')->default(0);
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('image');
             $table->timestamps();
         });
     }
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 };
---
+++ JJKirana/database/migrations/2023_05_21_090914_create_settings_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('type')->comment('like privacy page content, help page content, configration mail api and site name , logo');
+            $table->string('string')->comment('array formate like : array');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('settings');
+    }
+};
