af33481420e8260b82a493e1c3b5a8d783a66c30
Keval Bhanushali <bhanushalikeval51@gmail.com>
2023-06-06 22:07:22 +0530

    add carousel

--- JJKirana/resources/views/components/orders/list-card.blade.php
+++ JJKirana/resources/views/components/orders/list-card.blade.php
@@ -76,13 +76,27 @@
             </div>
             <div class="col-lg-12">
                 <div class="row">
-                    <div class="new-slider slider-arrow">
-                        <x-products.cards.third id="0" />
-                        <x-products.cards.third id="0" />
-                        <x-products.cards.third id="0" />
+                    <div class="section suggest-part">
+                        <div class="suggest-slider slider-arrow">
+                            <x-products.cards.third id="0" />
+                            <x-products.cards.third id="0" />
+                            <x-products.cards.third id="0" />
+                            <x-products.cards.third id="0" />
+
+                            <!-- Add carousel controls -->
+                            <a class="carousel-control-prev" href="#carouselExample" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExample" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 </div>
--- JJKirana/resources/views/components/products/cards/third.blade.php
+++ JJKirana/resources/views/components/products/cards/third.blade.php
@@ -1,19 +1,21 @@
-<div class="col-lg-4">
-    <figure class="snip1195">
-        <a href="{{ route('products.index') }}">
-            <h4>Cucumber</h4>
-            <div class="image">
-                <img src="{{ asset('storage/images/product/03.jpg') }}" alt="sq-sample4" />
-            </div>
-            <figcaption>
-                <div class="product-info">
-                    <div><strong>No:</strong> {{ $id }}</div>
-                    <div><strong>Product:</strong> Ice-Cream</div>
-                    <div><strong>Brand:</strong> Greeny</div>
-                    <div><strong>Price:</strong> ₹100</div>
-                    <div><strong>Quantity:</strong> 2</div>
+<div class="carousel-item">
+    <div class="col-lg-4">
+        <figure class="snip1195">
+            <a href="{{ route('products.index') }}">
+                <h4>Cucumber</h4>
+                <div class="image">
+                    <img src="{{ asset('storage/images/product/03.jpg') }}" />
                 </div>
-            </figcaption>
-        </a>
-    </figure>
+                <figcaption>
+                    <div class="product-info">
+                        <div><strong>No:</strong> {{ $id }}</div>
+                        <div><strong>Product:</strong> Ice-Cream</div>
+                        <div><strong>Brand:</strong> Vanilla</div>
+                        <div><strong>Price:</strong> ₹20</div>
+                        <div><strong>Quantity:</strong> 2</div>
+                    </div>
+                </figcaption>
+            </a>
+        </figure>
+    </div>
 </div>
